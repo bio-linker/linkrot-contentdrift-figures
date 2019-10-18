c3b7e9c29f978f54cbe05e97925338d605f1c727
jhpoelen <jhpoelen@xs4all.nl>
2019-10-17 18:51:04 -0700

    add option to patch all versions, not just most recent one. related to #1

--- patch-qualified-generations.py
+++ patch-qualified-generations.py
@@ -2,12 +2,9 @@ import re
 from uuid import uuid4 as UUID
 import sys
 
-from nquads import NQuads
-from prestongraph import *
-
 
 def main():
-
+    patchAll = True # enable this to patch *all* versions, not just the most recent (see https://github.com/bio-linker/linkrot-contentdrift-figures/issues/1)
     url = None
     latestVersion = None
     crawlUUID = None
@@ -15,6 +12,7 @@ def main():
         nQuads = NQuads.Parse(line)
         for nQuad in nQuads:
             triple = Triple.FromNQuad(nQuad)
+
             # Watch for newer versions for the current URL
             if (
                 triple.subject.Type() == Value.Type.CONTENT and
@@ -22,6 +20,8 @@ def main():
                 triple.object.Type() == Value.Type.CONTENT
             ):
                 latestVersion = str(triple.subject)
+                if patchAll:
+                    PrintQualifiedGeneration(url, latestVersion, crawlUUID)
             # Watch for existing qualified generations
             elif (
                 triple.subject.Type() == Value.Type.CONTENT and
@@ -38,12 +38,14 @@ def main():
                 triple.object.Type() == Value.Type.CONTENT
             ):
                 # Create a generation for the previous
-                if url and latestVersion:
+                if not patchAll and url and latestVersion:
                     PrintQualifiedGeneration(url, latestVersion, crawlUUID)
 
                 # Start reading triples for the next URL
                 url = str(triple.subject)
                 latestVersion = str(triple.object)
+                if patchAll:
+                    PrintQualifiedGeneration(url, latestVersion, crawlUUID)
             # Check for a new crawl UUID
             elif triple.object == "http://www.w3.org/ns/prov#Activity":
                 crawlUUID = str(triple.subject)
@@ -73,6 +75,281 @@ def PrintQualifiedGeneration(url, latestVersion, crawlUUID):
 
 #=============================================================================#
 
+class NQuads:
+    delimiters = {
+        '<' : '>',
+        '"' : '"'
+    }
+
+    # TODO: retain the "@en" flag on text values
+    def Parse(text):
+        nquads = []
+        groups = []
+        inGroup = False
+        subgroupStart = None
+        subgroups = []
+        delimiter = ''
+        for i, c in enumerate(text):
+            if inGroup:
+                if c == delimiter:
+                    subgroup = text[subgroupStart : i]
+                    subgroups.append(subgroup)
+                    delimiter = ''
+                elif delimiter == '':
+                    # Treat back-to-back delimiters as one group
+                    if c in NQuads.delimiters:
+                        delimiter = NQuads.delimiters[c]
+                        subgroupStart = i + 1
+                    # Spaces only end the group when outside a pair of delimiters
+                    elif c.isspace():
+                        groups.append(tuple(subgroups))
+                        inGroup = False
+                        subgroups = []
+            else:
+                if c == '.':
+                    nquads.append(groups)
+                    groups = []
+                elif c in NQuads.delimiters:
+                    delimiter = NQuads.delimiters[c]
+                    subgroupStart = i + 1
+                    inGroup = True
+        return nquads
+
+#=============================================================================#
+
+class Value:
+    from enum import Enum
+    class Type(Enum):
+        ANY     = 0
+        CONTENT = 1
+        HASH    = CONTENT
+        UUID    = 2
+        URL     = 3
+        RAW     = 4
+
+    def __init__(self, text, valueType):
+        assert type(text) == str
+        assert type(valueType) == Value.Type
+        self.text = text
+        self.type = valueType
+    
+    def __lt__(self, other):
+        return str(self) < str(other)
+
+    def __eq__(self, other):
+        t = type(other)
+        if   t == str:
+            return self.text == other
+        elif t == Value.Type:
+            return (other == Value.Type.ANY or self.type == other)
+        elif t == Value:
+            return (
+                self.text == other.text and
+                self.type == other.type
+            )
+        else:
+            return False
+
+    def __str__(self):
+        return self.text
+    
+    def IsHash(self):
+        return self.type == Value.Type.CONTENT
+
+    def FromText(text):
+        if (re.match('^hash:\/\/sha256\/.{64}$', text) or
+            re.match('^https?:\/\/.*\.well-known\/genid\/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$', text)):
+            type = Value.Type.CONTENT
+        elif re.match('^\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$', text):
+            type = Value.Type.UUID
+        elif re.match('^https?://', text):
+            type = Value.Type.URL
+        else:
+            type = Value.Type.RAW
+
+        return Value(text, type)
+
+#=============================================================================#
+
+class Verb:
+
+    def __init__(self, value):
+        assert type(value) == Value
+        self.value = value
+        self.triples = []
+    
+    def __lt__(self, other):
+        return str(self) < str(other)
+    
+    def __eq__(self, other):
+        t = type(other)
+        if  t == str:
+            return self.value == other
+        else:
+            return self.value == other.value
+
+    def __str__(self):
+        return self.value.text
+
+    def Text(self):
+        return self.value.text
+
+    def FromText(text, index=None):
+        """Returns a node associated with *text*"""
+        text = text.strip()
+        
+        # If the text is already indexed, use that
+        if index and text in index.verbLookup:
+            return index.verbLookup[text]
+
+        value = Value.FromText(text)
+        verb = Verb(value)
+
+        # Update the index
+        if index:
+            index.verbLookup[text] = verb
+            index.verbs.append(verb)
+
+        return verb
+
+#=============================================================================#
+
+class Node:
+
+    def __init__(self, value=None):
+        self.value = value
+        self.inwardTriples = []
+        self.outwardTriples = []
+    
+    def __lt__(self, other):
+        return str(self) < str(other)
+    
+    def __eq__(self, other):
+        t = type(other)
+        if  t == str:
+            return self.value == other
+        else:
+            return (
+                self.value == other.value
+            )
+
+    def __str__(self):
+        return self.value.text
+
+    def Text(self):
+        return self.value.text
+
+    def Type(self):
+        return self.value.type
+
+    def IsHash(self):
+        return self.value.type == Value.Type.CONTENT
+
+    def FromText(text, index=None):
+        """Returns a node associated with *text*"""
+        text = text.strip()
+        
+        # If the text is already indexed, use that
+        if index and text in index.nodeLookup:
+            return index.nodeLookup[text]
+
+        value = Value.FromText(text)
+        node = Node()
+        node.value = value
+
+        # Update the index
+        if index:
+            index.nodeLookup[text] = node
+            index.nodes.append(node)
+
+        return node
+
+#=============================================================================#
+
+class Triple:
+
+    def __init__(self, subject, verb, object):
+        assert type(subject) == Node
+        assert type(verb) == Verb
+        assert type(object) == Node
+        self.subject = subject
+        self.verb = verb
+        self.object = object
+    
+    def __lt__(self, other):
+        return str(self) < str(other)
+
+    def __eq__(self, other):
+        return (
+            self.subject    == other.subject    and
+            self.verb       == other.verb       and
+            self.object     == other.object
+        )
+
+    def __str__(self):
+        return str(self.subject) + '\t' + str(self.verb) + '\t' + str(self.object)
+
+    def Subject(self):
+        return parts[0]
+
+    def Verb(self):
+        return parts[1]
+
+    def Object(self):
+        return parts[2]
+
+    def Matches(self, subject=None, verb=None, object=None):
+        return (
+            (subject    == None or self.subject == subject  ) and
+            (verb       == None or self.verb    == verb     ) and
+            (object     == None or self.object  == object   )
+        )
+
+    def FromNQuad(nQuad, index=None):
+        """Returns a triple extracted from *nQuad*"""
+
+        nQuadString = str(nQuad)
+
+        # If the text is already indexed, use that
+        if index and nQuadString in index.tripleLookup:
+            return index.tripleLookup[nQuadString]
+
+        subject = Node.FromText(nQuad[0][0], index)
+        verb = Verb.FromText(nQuad[1][0], index)
+        object = Node.FromText(nQuad[2][0], index)
+
+        triple = Triple(subject, verb, object)
+
+        # Make connections
+        subject.outwardTriples.append(triple)
+        object.inwardTriples.append(triple)
+        verb.triples.append(triple)
+
+        # Update the index
+        if index:
+            index.tripleLookup[nQuadString] = triple
+            index.triples.append(triple)
+
+        return triple
+
+#=============================================================================#
+
+class Index:
+    def __init__(self, nQuads):
+        self.nodeLookup = dict()
+        self.verbLookup = dict()
+        self.tripleLookup = dict()
+
+        self.nodes = list()
+        self.verbs = list()
+        self.triples = list()
+
+        # Parse n-quads
+        for nQuad in nQuads:
+            Triple.FromNQuad(nQuad, index=self)
+
+#=============================================================================#
+
 if __name__ == "__main__":
     file = None
     if len(sys.argv) > 1:
@@ -85,278 +362,3 @@ if __name__ == "__main__":
     
     if file:
         file.close()
-
-# class NQuads:
-#     delimiters = {
-#         '<' : '>',
-#         '"' : '"'
-#     }
-
-#     # TODO: retain the "@en" flag on text values
-#     def Parse(text):
-#         nquads = []
-#         groups = []
-#         inGroup = False
-#         subgroupStart = None
-#         subgroups = []
-#         delimiter = ''
-#         for i, c in enumerate(text):
-#             if inGroup:
-#                 if c == delimiter:
-#                     subgroup = text[subgroupStart : i]
-#                     subgroups.append(subgroup)
-#                     delimiter = ''
-#                 elif delimiter == '':
-#                     # Treat back-to-back delimiters as one group
-#                     if c in NQuads.delimiters:
-#                         delimiter = NQuads.delimiters[c]
-#                         subgroupStart = i + 1
-#                     # Spaces only end the group when outside a pair of delimiters
-#                     elif c.isspace():
-#                         groups.append(tuple(subgroups))
-#                         inGroup = False
-#                         subgroups = []
-#             else:
-#                 if c == '.':
-#                     nquads.append(groups)
-#                     groups = []
-#                 elif c in NQuads.delimiters:
-#                     delimiter = NQuads.delimiters[c]
-#                     subgroupStart = i + 1
-#                     inGroup = True
-#         return nquads
-
-# #=============================================================================#
-
-# class Value:
-#     from enum import Enum
-#     class Type(Enum):
-#         ANY     = 0
-#         CONTENT = 1
-#         HASH    = CONTENT
-#         UUID    = 2
-#         URL     = 3
-#         RAW     = 4
-
-#     def __init__(self, text, valueType):
-#         assert type(text) == str
-#         assert type(valueType) == Value.Type
-#         self.text = text
-#         self.type = valueType
-    
-#     def __lt__(self, other):
-#         return str(self) < str(other)
-
-#     def __eq__(self, other):
-#         t = type(other)
-#         if   t == str:
-#             return self.text == other
-#         elif t == Value.Type:
-#             return (other == Value.Type.ANY or self.type == other)
-#         elif t == Value:
-#             return (
-#                 self.text == other.text and
-#                 self.type == other.type
-#             )
-#         else:
-#             return False
-
-#     def __str__(self):
-#         return self.text
-    
-#     def IsHash(self):
-#         return self.type == Value.Type.CONTENT
-
-#     def FromText(text):
-#         if (re.match('^hash:\/\/sha256\/.{64}$', text) or
-#             re.match('^https?:\/\/.*\.well-known\/genid\/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$', text)):
-#             type = Value.Type.CONTENT
-#         elif re.match('^\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$', text):
-#             type = Value.Type.UUID
-#         elif re.match('^https?://', text):
-#             type = Value.Type.URL
-#         else:
-#             type = Value.Type.RAW
-
-#         return Value(text, type)
-
-# #=============================================================================#
-
-# class Verb:
-
-#     def __init__(self, value):
-#         assert type(value) == Value
-#         self.value = value
-#         self.triples = []
-    
-#     def __lt__(self, other):
-#         return str(self) < str(other)
-    
-#     def __eq__(self, other):
-#         t = type(other)
-#         if  t == str:
-#             return self.value == other
-#         else:
-#             return self.value == other.value
-
-#     def __str__(self):
-#         return self.value.text
-
-#     def Text(self):
-#         return self.value.text
-
-#     def FromText(text, index=None):
-#         """Returns a node associated with *text*"""
-#         text = text.strip()
-        
-#         # If the text is already indexed, use that
-#         if index and text in index.verbLookup:
-#             return index.verbLookup[text]
-
-#         value = Value.FromText(text)
-#         verb = Verb(value)
-
-#         # Update the index
-#         if index:
-#             index.verbLookup[text] = verb
-#             index.verbs.append(verb)
-
-#         return verb
-
-# #=============================================================================#
-
-# class Node:
-
-#     def __init__(self, value=None):
-#         self.value = value
-#         self.inwardTriples = []
-#         self.outwardTriples = []
-    
-#     def __lt__(self, other):
-#         return str(self) < str(other)
-    
-#     def __eq__(self, other):
-#         t = type(other)
-#         if  t == str:
-#             return self.value == other
-#         else:
-#             return (
-#                 self.value == other.value
-#             )
-
-#     def __str__(self):
-#         return self.value.text
-
-#     def Text(self):
-#         return self.value.text
-
-#     def Type(self):
-#         return self.value.type
-
-#     def IsHash(self):
-#         return self.value.type == Value.Type.CONTENT
-
-#     def FromText(text, index=None):
-#         """Returns a node associated with *text*"""
-#         text = text.strip()
-        
-#         # If the text is already indexed, use that
-#         if index and text in index.nodeLookup:
-#             return index.nodeLookup[text]
-
-#         value = Value.FromText(text)
-#         node = Node()
-#         node.value = value
-
-#         # Update the index
-#         if index:
-#             index.nodeLookup[text] = node
-#             index.nodes.append(node)
-
-#         return node
-
-# #=============================================================================#
-
-# class Triple:
-
-#     def __init__(self, subject, verb, object):
-#         assert type(subject) == Node
-#         assert type(verb) == Verb
-#         assert type(object) == Node
-#         self.subject = subject
-#         self.verb = verb
-#         self.object = object
-    
-#     def __lt__(self, other):
-#         return str(self) < str(other)
-
-#     def __eq__(self, other):
-#         return (
-#             self.subject    == other.subject    and
-#             self.verb       == other.verb       and
-#             self.object     == other.object
-#         )
-
-#     def __str__(self):
-#         return str(self.subject) + '\t' + str(self.verb) + '\t' + str(self.object)
-
-#     def Subject(self):
-#         return parts[0]
-
-#     def Verb(self):
-#         return parts[1]
-
-#     def Object(self):
-#         return parts[2]
-
-#     def Matches(self, subject=None, verb=None, object=None):
-#         return (
-#             (subject    == None or self.subject == subject  ) and
-#             (verb       == None or self.verb    == verb     ) and
-#             (object     == None or self.object  == object   )
-#         )
-
-#     def FromNQuad(nQuad, index=None):
-#         """Returns a triple extracted from *nQuad*"""
-
-#         nQuadString = str(nQuad)
-
-#         # If the text is already indexed, use that
-#         if index and nQuadString in index.tripleLookup:
-#             return index.tripleLookup[nQuadString]
-
-#         subject = Node.FromText(nQuad[0][0], index)
-#         verb = Verb.FromText(nQuad[1][0], index)
-#         object = Node.FromText(nQuad[2][0], index)
-
-#         triple = Triple(subject, verb, object)
-
-#         # Make connections
-#         subject.outwardTriples.append(triple)
-#         object.inwardTriples.append(triple)
-#         verb.triples.append(triple)
-
-#         # Update the index
-#         if index:
-#             index.tripleLookup[nQuadString] = triple
-#             index.triples.append(triple)
-
-#         return triple
-
-# #=============================================================================#
-
-# class Index:
-#     def __init__(self, nQuads):
-#         self.nodeLookup = dict()
-#         self.verbLookup = dict()
-#         self.tripleLookup = dict()
-
-#         self.nodes = list()
-#         self.verbs = list()
-#         self.triples = list()
-
-#         # Parse n-quads
-#         for nQuad in nQuads:
-#             Triple.FromNQuad(nQuad, index=self)
-
-# #=============================================================================#
