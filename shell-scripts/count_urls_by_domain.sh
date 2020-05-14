532f83a68f6aab00a7049ce171097c869f21036f
Michael Elliott <mielliott@ufl.edu>
2020-05-14 13:44:29 -0400

    Clean up

--- shell-scripts/count_urls_by_domain.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cat tmp/urls | sed -r 's/^(https?:\/\/[^/]+\/).*/\1/g' | sort | uniq -c | sort -nr > tmp/url_counts_by_domain
--- shell-scripts/stage_crawl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-HASH=$1
-DIR=tmp_crawls/$HASH
-
-mkdir -p $DIR
-
-# TODO: all spaces are currently being replaced by tabs -- this is bad; spaces within "" or <> delimiters should be preserved. For our current purposes, though, this is inconsequential
-preston get --remote http://preston.acis.ufl.edu hash://sha256/$HASH | sed -r 's/<([^>]*)>/\1/g' | tr ' ' '\t' | cut -f 1,2,3 > $DIR/$HASH
